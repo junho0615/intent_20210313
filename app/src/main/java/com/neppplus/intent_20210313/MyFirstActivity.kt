6da15fd636d29cf496ad6ee5a716702c40f7b157
홍준호 <0615wnsgh@gmail.com>
2021-03-13 12:32:58 +0900

    이전 복귀버튼 => Intent 붙여둠

--- app/src/main/java/com/neppplus/intent_20210313/MyFirstActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/MyFirstActivity.kt
@@ -3,22 +3,17 @@ package com.neppplus.intent_20210313
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_my_first.*
 
-class MainActivity : AppCompatActivity() {
+class MyFirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_my_first)
 
-        moveToFirstBtn.setOnClickListener {
+        backToMainBtn.setOnClickListener {
 
-            val myIntent = Intent(this, MyFirstActivity::class.java)
-
+            val myIntent = Intent(this, MainActivity::class.java)
             startActivity(myIntent)
-
-            }
-
         }
     }
 }
